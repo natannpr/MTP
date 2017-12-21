8c3847bc6c40d365494a85d4011551f32f5b98da
natannpr <natan.paranaiba@hotmail.com>
2017-12-20 22:22:58 -0200

    correção realizada
    correção realizada

---
+++ aula20171018/rec2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+#include <string.h>
+
+void inverter (char *frase)
+{
+	char str1[100];
+	int a;
+	printf ("\n A frase invertida sera:");
+	for(a = strlen(frase) -1; a >= 0; a--)
+		printf ("%c", frase[a]);
+}
+
+
+int main()
+{
+	char frase[100];
+	printf (" Digite sua frase:");
+	gets(frase);
+	inverter(frase);
+	getche ();
+	return 0;
+}
